b0ff4a572d99b640709379a7f35df71117d9b86a
Miles McArdle-Coe <email": null>
2016-11-19 16:06:56 -0500

    heroku push after pg swap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Post.delete_all
+Comment.delete_all
+
+users = 10.times.map do
+  User.create!( :username => Faker::Internet.user_name,
+                :email => Faker::Internet.email,
+                :password => "password" )
+end
+
+posts = 30.times.map do
+  Post.create!( :title => Faker::StarWars.quote,
+                :content => Faker::Hipster.paragraph,
+                :user_id => (1..10).to_a.sample )
+end
+
+comments = 200.times.map do
+  Comment.create!( :reply => Faker::Hacker.say_something_smart,
+                   :post_id => (1..30).to_a.sample,
+                   :user_id => (1..10).to_a.sample )
+end
